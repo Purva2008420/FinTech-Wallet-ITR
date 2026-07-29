02ac7a3795e5aeef1743597a845246ec6e4965b7
Purva Hase <your.real.purvahase24@gmail.com>
2026-07-30 00:11:35 +0530

    Configured PostgreSQL and created project apps

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
+    "wallet",
+    "transactions",
+    "fraud_detection",
 ]
 
 MIDDLEWARE = [
