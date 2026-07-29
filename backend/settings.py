5c40939c423fe98e1f4bb4f7bb6d8113b28bc55e
Purva Hase <your.real.purvahase24@gmail.com>
2026-07-30 01:58:19 +0530

    Successfully built and verified User Registration API with Custom User model

--- backend/settings.py
+++ backend/settings.py
@@ -37,10 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "accounts",
-    "wallet",
-    "transactions",
-    "fraud_detection",
+    'rest_framework',
+    'corsheaders',
+    'accounts',
+    'wallet',
+    'transactions',
+    'fraud_detection',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +80,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fintech_wallet_v2',  # <-- Change this to v2
+        'USER': 'postgres',
+        'PASSWORD': '2634',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -119,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'accounts.User'
