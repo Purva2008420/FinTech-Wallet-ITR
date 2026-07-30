f80dfec87fedbae5047663e58c6c974e18cdca91
Purva Hase <your.real.purvahase24@gmail.com>
2026-07-30 18:10:43 +0530

    Implemented JWT authentication and protected profile API

--- accounts/views.py
+++ accounts/views.py
@@ -5,3 +5,16 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "username": request.user.username,
+            "email": request.user.email,
+            "phone": request.user.phone
+        })
